8ebbd748963dbff67973ae5531e1a3ce44c235cd
atikazahraa <atikazahraa02@gmail.com>
2022-12-05 07:28:21 +0700

    Atika Zahra_H1D020087_Intent Score

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -1,15 +1,77 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int HOME_REQUEST_CODE = 1;
+    private static final int AWAY_REQUEST_CODE = 2;
+    private static final String TAG = MainActivity.class.getCanonicalName();
+
+    private String hometeam;
+    private String awayteam;
+    private EditText inphome;
+    private EditText inpaway;
+    private ImageView logohome;
+    private ImageView logoaway;
+    private Button buttomteam;
+    private Uri imghome;
+    private Uri imgaway;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inphome = findViewById(R.id.home_team);
+        inpaway = findViewById(R.id.away_team);
+        logohome = findViewById(R.id.home_logo);
+        logoaway = findViewById(R.id.away_logo);
+        buttomteam = findViewById(R.id.btn_team);
+
+        buttomteam.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                hometeam = inphome.getText().toString();
+                awayteam = inpaway.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MatchActivity.class);
+                intent.putExtra("namahome",hometeam);
+                intent.putExtra("namaway", awayteam);
+                intent.putExtra("imghome", imghome.toString());
+                intent.putExtra("imgaway", imgaway.toString());
+                startActivity(intent);
+            }
+        });
+
+        logohome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), HOME_REQUEST_CODE);
+            }
+        });
+
+        logoaway.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), AWAY_REQUEST_CODE);
+            }
+        });
+
         //TODO
         //Fitur Main Activity
         //1. Validasi Input Home Team
@@ -17,5 +79,42 @@ public class MainActivity extends AppCompatActivity {
         //3. Ganti Logo Home Team
         //4. Ganti Logo Away Team
         //5. Next Button Pindah Ke MatchActivity
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_CANCELED){
+            Log.d(TAG, "Pilih gambar dicancel");
+            return;
+        }
+        else if(requestCode == HOME_REQUEST_CODE){
+            if(data != null){
+                try {
+                    Uri imageUri = data.getData();
+                    imghome = imageUri;
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    logohome.setImageBitmap(bitmap);
+                }
+                catch (IOException error){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, error.getMessage());
+                }
+            }
+        }
+        else if(requestCode == AWAY_REQUEST_CODE){
+            if(data != null){
+                try {
+                    Uri imageUri = data.getData();
+                    imgaway = imageUri;
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    logoaway.setImageBitmap(bitmap);
+                }
+                catch (IOException error){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, error.getMessage());
+                }
+            }
+        }
     }
 }
--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,14 +2,95 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
+
+    private String hometeam;
+    private String awayteam;
+    private String pemenang;
+    private int skorhome;
+    private int skoraway;
+    private TextView tvskorhome;
+    private TextView tvskoraway;
+    private TextView tvhome;
+    private TextView tvaway;
+    private ImageView logohome;
+    private ImageView logoaway;
+    private Button addHome;
+    private Button addAway;
+    private Button cekhasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        tvhome = findViewById(R.id.txt_home);
+        tvaway = findViewById(R.id.txt_away);
+        skorhome = findViewById(R.id.score_home);
+        skoraway = findViewById(R.id.score_away);
+        logohome = findViewById(R.id.home_logo);
+        logoaway = findViewById(R.id.away_logo);
+        addHome = findViewById(R.id.btn_add_home);
+        addAway = findViewById(R.id.btn_add_away);
+        cekhasil = findViewById(R.id.btn_result);
+
+        skorhome = 0;
+        skoraway = 0;
+        tvskorhome.setText(String.valueOf(skorhome));
+        tvskoraway.setText(String.valueOf(skoraway));
+
+        Bundle bundle = getIntent().getExtras();
+        hometeam = bundle.getString("namahome");
+        tvhome.setText(hometeam);
+        awayteam = bundle.getString("namaaway");
+        tvaway.setText(awayteam);
+        logohome.setImageURI(Uri.parse(bundle.getString("homeImg")));
+        logoaway.setImageURI(Uri.parse(bundle.getString("awayImg")));
+
+        addHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skorhome += 1;
+                tvskorhome.setText(String.valueOf(skorhome));
+            }
+        });
+
+        addAway.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skoraway += 1;
+                tvskoraway.setText(String.valueOf(skoraway));
+            }
+        });
+
+        cekhasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pemenang = "empty";
+                if(skorhome > skoraway){
+                    pemenang = hometeam;
+                }
+                else if (skorhome == skoraway){
+                    pemenang = "draw";
+                }
+                else {
+                    pemenang = awayteam;
+                }
+
+                Intent intent = new Intent(MatchActivity.this, ResultActivity.class);
+                intent.putExtra("winner", pemenang);
+                startActivity(intent);
+            }
+        });
+
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
         //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
